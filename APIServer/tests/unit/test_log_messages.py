a2f873e4fc6b4c6a36f083861247ef30837fec2f
Chris Donati <chris.donati@appscale.com>
2018-04-04 16:48:49 -0700

    update

--- APIServer/tests/unit/test_log_messages.py
+++ APIServer/tests/unit/test_log_messages.py
@@ -6,33 +6,48 @@ import time
 LOG_LEVEL_INFO = 1
 LOG_LEVEL_WARNING = 2
 
+MESSAGE_1 = 'show me what you got'
+
+TIMESTAMP_USEC = int(time.time() * 1000 * 1000)
+
 
 class TestAppLog(unittest.TestCase):
     def test_from_pb(self):
-        current_time_us = int(time.time() * 1000 * 1000)
-        message = 'this is fine'
-
         # Test creating an AppLog from a LogLine.
         line_pb = log_service_pb2.LogLine()
-        line_pb.time = current_time_us
+        line_pb.time = TIMESTAMP_USEC
         line_pb.level = LOG_LEVEL_INFO
-        line_pb.log_message = message
+        line_pb.log_message = MESSAGE_1
         app_log = AppLog.from_pb(line_pb)
-        self.assertEqual(app_log.time, current_time_us)
+        self.assertEqual(app_log.time, TIMESTAMP_USEC)
         self.assertEqual(app_log.level, LOG_LEVEL_INFO)
-        self.assertEqual(app_log.message, message)
+        self.assertEqual(app_log.message, MESSAGE_1)
 
         # Test creating an AppLog from a UserAppLogLine.
         line_pb = log_service_pb2.UserAppLogLine()
-        line_pb.timestamp_usec = current_time_us
+        line_pb.timestamp_usec = TIMESTAMP_USEC
         line_pb.level = LOG_LEVEL_WARNING
-        line_pb.message = message
+        line_pb.message = MESSAGE_1
         app_log = AppLog.from_pb(line_pb)
-        self.assertEqual(app_log.time, current_time_us)
+        self.assertEqual(app_log.time, TIMESTAMP_USEC)
         self.assertEqual(app_log.level, LOG_LEVEL_WARNING)
-        self.assertEqual(app_log.message, message)
+        self.assertEqual(app_log.message, MESSAGE_1)
 
-        # Test encoding to capnp.
-        app_log = AppLog('asdf', LOG_LEVEL_INFO, message)
+    def test_to_capnp(self):
+        app_log = AppLog(TIMESTAMP_USEC, LOG_LEVEL_INFO, MESSAGE_1)
         line_capnp = app_log.to_capnp()
+        self.assertEqual(line_capnp.time, TIMESTAMP_USEC)
+        self.assertEqual(line_capnp.level, LOG_LEVEL_WARNING)
+        self.assertEqual(line_capnp.message, MESSAGE_1)
+        # Ensure no exceptions are raised when serializing message.
         line_capnp.encoded_bytes()
+
+    def test_to_log_line(self):
+        # Test encoding to protobuffer.
+        app_log = AppLog(TIMESTAMP_USEC, LOG_LEVEL_INFO, MESSAGE_1)
+        line_pb = app_log.to_log_line()
+        self.assertEqual(line_pb.time, TIMESTAMP_USEC)
+        self.assertEqual(line_pb.level, LOG_LEVEL_WARNING)
+        self.assertEqual(line_pb.log_message, MESSAGE_1)
+        # Ensure no exceptions are raised when serializing message.
+        line_pb.SerializeToString()
