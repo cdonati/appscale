8efbdbd741fa767a0a5c5421ce0ebe0adea3730c
Chris Donati <chris.donati@appscale.com>
2019-03-20 20:50:20 -0700

    update

--- AppDB/appscale/datastore/fdb/view_records.py
+++ AppDB/appscale/datastore/fdb/view_records.py
@@ -1,5 +1,7 @@
 import fdb
 import struct
+import sys
+import tabulate
 
 fdb.api_version(600)
 
@@ -8,18 +10,54 @@ db = fdb.open()
 ds_dir = fdb.directory.open(db, ('appscale', 'datastore'))
 
 
+def format_path(path):
+  index = 0
+  elements = []
+  while index < len(path):
+    kind = path[index]
+    id_or_name = path[index + 1]
+    elements.append(':'.join([kind, str(id_or_name)]))
+    index += 2
+
+  return '|'.join(elements)
+
+
 def print_data(tr, data_dir):
   namespaces = data_dir.list(tr)
   for namespace in namespaces:
     namespace_dir = data_dir.open(tr, (namespace,))
-    print('/'.join(namespace_dir.get_path()) + ':')
+    project_id, section_id, pretty_ns = namespace_dir.get_path()[2:]
+    if pretty_ns == '':
+      pretty_ns = '""'
+
+    print('/'.join([project_id, section_id, pretty_ns]) + ':')
+    headers = ['Path', 'Versionstamp', 'Entity Version', 'Entity']
+    table = []
+
+    tmp_chunks = []
     for kv in tr[namespace_dir.range()]:
       key_parts = namespace_dir.unpack(kv.key)
-      path = key_parts[:-2]
-      versionstamp = struct.unpack('>Q', key_parts[-2].tr_version[:8])[0]
-      index = key_parts[-1]
       value_parts = fdb.tuple.unpack(kv.value)
-      print('{} {} {}: {}'.format(path, versionstamp, index, value_parts))
+
+      path = format_path(key_parts[:-2])
+      versionstamp = struct.unpack('>Q', key_parts[-2].tr_version[:8])[0]
+      entity_version = value_parts[0]
+      entity_chunk = value_parts[2]
+      index = key_parts[-1]
+      if index != 0:
+        tmp_chunks.append(entity_chunk)
+      else:
+        if tmp_chunks:
+          encoded_entity = ''.join(tmp_chunks)
+          table.append([path, versionstamp, entity_version, encoded_entity])
+          tmp_chunks = [entity_chunk]
+
+      if tmp_chunks:
+          encoded_entity = ''.join(tmp_chunks)
+          table.append([path, versionstamp, entity_version, encoded_entity])
+          tmp_chunks = [entity_chunk]
+
+      print(tabulate.tabulate(table, headers=headers))
 
 
 def print_indexes(tr, indexes_dir):
