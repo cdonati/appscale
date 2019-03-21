387b6922da82f2e567d864890db04c69766ffdf5
Chris Donati <chris.donati@appscale.com>
2019-03-20 20:12:46 -0700

    update

--- AppDB/appscale/datastore/fdb/view_records.py
+++ AppDB/appscale/datastore/fdb/view_records.py
@@ -7,5 +7,8 @@ db = fdb.open()
 ds_dir = fdb.directory.open(db, ('appscale', 'datastore'))
 
 def main():
-  for project in ds_dir.list(db):
-    print(project)
+  for project_id in ds_dir.list(db):
+    print(project_id)
+    project_dir = ds_dir.open(db, (project_id,))
+    for section_id in project_dir.list(db):
+      print('--' + section_id)
