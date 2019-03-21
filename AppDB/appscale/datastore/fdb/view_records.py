a09c5b697dc06cdc54345163c9b431e5f1f91fb3
Chris Donati <chris.donati@appscale.com>
2019-03-20 20:09:10 -0700

    update

---
+++ AppDB/appscale/datastore/fdb/view_records.py
@@ -0,0 +1,11 @@
+import fdb
+
+fdb.api_version(600)
+
+db = fdb.open()
+
+ds_dir = db.directory.open(db, ('appscale', 'datastore'))
+
+def main():
+  for project in ds_dir.list():
+    print(project)
