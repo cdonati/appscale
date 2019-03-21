fa6c0f9d0892ae67c38fb379cd836a1fb24ab2ed
Chris Donati <chris.donati@appscale.com>
2019-03-20 20:18:36 -0700

    update

--- AppDB/appscale/datastore/fdb/view_records.py
+++ AppDB/appscale/datastore/fdb/view_records.py
@@ -6,9 +6,29 @@ db = fdb.open()
 
 ds_dir = fdb.directory.open(db, ('appscale', 'datastore'))
 
+
+def print_data(tr, data_dir):
+  namespaces = data_dir.list(tr)
+  for namespace in namespaces:
+    print('---- namespace: "{}"'.format(namespace))
+
+
+def print_indexes(tr, indexes_dir):
+  namespaces = indexes_dir.list(tr)
+  for namespace in namespaces:
+    print('---- namespace: "{}"'.format(namespace))
+
+
 def main():
-  for project_id in ds_dir.list(db):
+  tr = db.create_transaction()
+  for project_id in ds_dir.list(tr):
     print(project_id)
-    project_dir = ds_dir.open(db, (project_id,))
-    for section_id in project_dir.list(db):
+    project_dir = ds_dir.open(tr, (project_id,))
+    for section_id in project_dir.list(tr):
       print('--' + section_id)
+      section_dir = ds_dir.open(tr, (project_id,))
+      if section_id == 'data':
+        print_data(tr, section_dir)
+
+      if section_id == 'indexes':
+        print_indexes(tr, section_dir)
