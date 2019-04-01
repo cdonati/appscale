d344334418769489c0cf30f27cddba638df0dfd8
Chris Donati <chris.donati@appscale.com>
2019-03-31 18:47:27 -0700

    update

--- AppDB/appscale/datastore/fdb/indexes.py
+++ AppDB/appscale/datastore/fdb/indexes.py
@@ -290,10 +290,10 @@ class IndexIterator(object):
 
     logger.debug('start offset: {}'.format(self.start_offset))
     logger.debug('stop offset: {}'.format(self.stop_offset))
-    logger.debug('start: {}'.format(self.start.key))
-    logger.debug('stop: {}'.format(self.stop.key))
+    logger.debug('start: {!r}'.format(self.start.key))
+    logger.debug('stop: {!r}'.format(self.stop.key))
     self._kv_iterator = KVIterator(
-      tr, tornado_fdb, (self.start, self.stop), fetch_limit, reverse,
+      tr, tornado_fdb, slice(self.start, self.stop), fetch_limit, reverse,
       snapshot=snapshot)
     if read_vs is None:
       read_vs = fdb.tuple.Versionstamp()
