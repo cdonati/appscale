481c6cc523f7a42323b03695a9a68a0504b9c56e
Chris Donati <chris.donati@appscale.com>
2019-04-04 17:22:32 -0700

    update

--- AppDB/appscale/datastore/fdb/gc.py
+++ AppDB/appscale/datastore/fdb/gc.py
@@ -1,7 +1,9 @@
 import logging
 import time
 from collections import deque
+
 from tornado import gen
+from tornado.ioloop import IOLoop
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +17,7 @@ class GarbageCollector(object):
     self._index_manager = index_manager
 
   def start(self):
-    pass
+    IOLoop.current().spawn_callback(self._run_forever)
 
   def clear_later(self, entities, gc_versionstamp):
     safe_time = time.time() + 60
@@ -23,41 +25,44 @@ class GarbageCollector(object):
       self._queue.append((safe_time, entity, gc_versionstamp))
 
   @gen.coroutine
-  def process_queue(self):
+  def _run_forever(self):
     while True:
       try:
-        logger.debug('processing queue')
-        current_time = time.time()
-        tx_deadline = current_time + 2.5
-        tr = None
-        while True:
-          if not self._queue:
-            if tr is not None:
-              yield self._tornado_fdb.commit(tr)
-
-            yield gen.sleep(61)
-            break
-
-          safe_time, entity, gc_versionstamp = self._queue.popleft()
-          if safe_time > current_time:
-            self._queue.appendleft((safe_time, entity, gc_versionstamp))
-            if tr is not None:
-              yield self._tornado_fdb.commit(tr)
-
-            yield gen.sleep(safe_time - current_time + 1)
-            break
-
-          if tr is None:
-            tr = self._db.create_transaction()
-
-          self._data_manager.hard_delete(tr, entity.key(), gc_versionstamp)
-          self._index_manager.hard_delete_entries(tr, entity, gc_versionstamp)
-
-          if time.time() > tx_deadline:
-            yield self._tornado_fdb.commit(tr)
-            break
-
+        yield self._process_queue()
       except Exception:
         # TODO: Exponential backoff here.
         logger.exception('Unexpected error while processing GC queue')
         continue
+
+  @gen.coroutine
+  def _process_queue(self):
+    logger.debug('processing queue')
+    current_time = time.time()
+    tx_deadline = current_time + 2.5
+    tr = None
+    while True:
+      if not self._queue:
+        if tr is not None:
+          yield self._tornado_fdb.commit(tr)
+
+        yield gen.sleep(61)
+        break
+
+      safe_time, entity, gc_versionstamp = self._queue.popleft()
+      if safe_time > current_time:
+        self._queue.appendleft((safe_time, entity, gc_versionstamp))
+        if tr is not None:
+          yield self._tornado_fdb.commit(tr)
+
+        yield gen.sleep(safe_time - current_time + 1)
+        break
+
+      if tr is None:
+        tr = self._db.create_transaction()
+
+      self._data_manager.hard_delete(tr, entity.key(), gc_versionstamp)
+      self._index_manager.hard_delete_entries(tr, entity, gc_versionstamp)
+
+      if time.time() > tx_deadline:
+        yield self._tornado_fdb.commit(tr)
+        break
