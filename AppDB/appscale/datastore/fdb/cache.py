65751cc53cf4b8cc7549aa87371c4473184f2335
Chris Donati <chris.donati@appscale.com>
2019-05-16 18:33:45 -0700

    update

--- AppDB/appscale/datastore/fdb/cache.py
+++ AppDB/appscale/datastore/fdb/cache.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from tornado import gen
 
 from appscale.datastore.dbconstants import InternalError
@@ -7,20 +9,31 @@ class DirectoryCache(object):
   # The root directory that the datastore uses.
   ROOT = ('appscale', 'datastore')
 
-  # The number of items the cache can hold.
-  SIZE = 1024
-
   # The location of the metadata version key.
   METADATA_KEY = b'\xff/metadataVersion'
 
-  def __init__(self, tornado_fdb):
+  def __init__(self, tornado_fdb, root_dir, cache_size):
+    self.root_dir = root_dir
+    self._cache_size = cache_size
     self._directory_dict = {}
-    self._directory_list = []
+    self._directory_keys = deque()
     self._metadata_version = None
     self._tornado_fdb = tornado_fdb
-    self._root_dir = None
 
-  def ensure_root(self, tr):
+  def __setitem__(self, key, value):
+    if key not in self._directory_dict:
+      self._directory_keys.append(key)
+      if len(self._directory_keys) > self._cache_size:
+        oldest_key = self._directory_keys.popleft()
+        del self._directory_dict[oldest_key]
+
+    self._directory_dict[key] = value
+
+  def __getitem__(self, key):
+    return self._directory_dict[key]
+
+  def __contains__(self, item):
+    return item in self._directory_dict
 
   @gen.coroutine
   def _validate_cache(self, tr):
@@ -30,8 +43,8 @@ class DirectoryCache(object):
 
     if current_version.value != self._metadata_version:
       self._metadata_version = current_version.value
-      self._directory_dict = {}
-      self._directory_list = []
+      self._directory_dict.clear()
+      self._directory_keys.clear()
 
   @gen.coroutine
   def _ensure_project(self, tr, project_id):
@@ -42,23 +55,20 @@ class DirectoryCache(object):
 
 
 class ProjectCache(DirectoryCache):
-  # The root directory that the datastore uses.
-  ROOT = ('appscale', 'datastore')
-
   # The number of items the cache can hold.
-  SIZE = 1024
+  SIZE = 256
 
-  # The location of the metadata version key.
-  METADATA_KEY = b'\xff/metadataVersion'
-
-  def __init__(self, tornado_fdb, root):
-    super(ProjectCache, self).__init__(tornado_fdb)
-    self._root = root
+  def __init__(self, tornado_fdb, root_dir):
+    super(ProjectCache, self).__init__(tornado_fdb, root_dir, self.SIZE)
 
   @gen.coroutine
   def get(self, tr, project_id):
+    # Ensure the project hasn't been deleted.
     yield self._validate_cache(tr)
+    if project_id not in self:
+      self[project_id] = self.root_dir.
     if project_id in self._directory_dict:
+      return
       
   @gen.coroutine
   def _validate_cache(self, tr):
